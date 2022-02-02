9f8d84d3099db868664cbdb66e7cb30749bf0025
mylena@usto.re <mylena@usto.re>
2022-02-02 04:59:06 -0300

    feat: criação de login

---
+++ src/app/validation/authenticaticate/login.js
@@ -0,0 +1,22 @@
+const Joi = require('joi');
+
+const UtilError = require('../../util/utilError');
+
+module.exports = async (req, res, next) => {
+    try {      
+        const schema = Joi.object({
+            email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'br'] } }),
+            senha: Joi.string().min(6).required()
+        });
+
+        const { error } = await schema.validate(req.body, { abortEarly: false });
+
+        if (error) {
+            throw UtilError.badRequest(res, error.details);
+        }
+
+        return next();
+    } catch (error) {
+        return UtilError.badRequest(res, error.message);
+    }
+}
