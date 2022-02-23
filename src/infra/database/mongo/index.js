5b60a5a4725bec9c5d7b353cf7e5b41ecd6e49db
mylena <mylena@usto.re>
2022-02-23 02:10:56 -0300

    feat: adição do atlas

--- src/infra/database/mongo/index.js
+++ src/infra/database/mongo/index.js
@@ -1,17 +1,26 @@
 const mongoose = require('mongoose');
+require('dotenv-safe').config();
 
 class Database {
   constructor() {
     this.connect();
   }
 
-  connect() {
+  async connect() {
     const db = process.env.DATABASE || 'mongodb://127.0.0.1:27017/compassolisa';
+
     return mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       autoIndex: true
-    });
+    })
+      .then(() => {
+        console.log("Conectado com o banco ", db)
+      })
+      .catch((error) => {
+        console.log("Algo deu errado")
+        console.error(error)
+      });
   }
 }
 
