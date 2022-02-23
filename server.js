f395819780e73bff85436ce52fddca14a9e5a0c5
mylena <mylena@usto.re>
2022-02-23 12:27:21 -0300

    feat: ajuste no atlas

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require('./app.js');
+const app = require('./src/app');
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
--- src/routers/peapleRouter.js
+++ src/routers/peapleRouter.js
@@ -1,4 +1,4 @@
-const PeapleController = require('../app/controllers/PeapleController');
+const PeapleController = require('src/app/controllers/PeapleController');
 
 const createValidation = require('../app/validation/peaple/create');
 const validationId = require('../app/validation/general/validationId.js');
